44bd725e933485534ef02fb215441f3f30549917
徐腾飞 <xutengfei@126.com>
2021-04-06 17:28:24 +0800

    增加下拉菜单组件

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GolbalHeader.vue'
 const currentUser: UserProps = {
-  isLogin: false
+  isLogin: true,
+  name: 'xu'
 }
 const testData: ColumnProps[] = [
   {
