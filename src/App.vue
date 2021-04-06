2189753b45ab6afcb6f2e33c09be3b865250ab77
xutengfei0820 <18515219314@163.com>
2021-04-07 00:29:39 +0800

    删除aa图片，增加input的校验以及公共方法

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+    <form>
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules"></validate-input>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        />
+        <div class="form-text" v-if="emailRef.error">
+          {{ emailRef.message }}
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+    </form>
+    <column-list :list="list" v-if="false"></column-list>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GolbalHeader.vue'
+import validateInput, { RulesProp } from './components/ValidateInput.vue'
 const currentUser: UserProps = {
   isLogin: true,
   name: 'xu'
@@ -42,14 +71,38 @@ const testData: ColumnProps[] = [
       'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
 ]
+const emailReg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
 export default defineComponent({
-  components: { ColumnList, GlobalHeader },
+  components: { ColumnList, GlobalHeader, validateInput },
   name: 'App',
   // eslint-disable-next-line space-before-function-paren
   setup() {
+    const emailRules: RulesProp = [
+      { type: 'required', message: '邮箱不能为空' },
+      { type: 'email', message: '请输入正确的邮箱' }
+    ]
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = '邮箱不能为空'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = '请输入正确的邮箱'
+      } else {
+        emailRef.error = false
+      }
+    }
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRef,
+      validateEmail,
+      emailRules
     }
   }
 })
--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="dropdown">
-    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
-      {{title}}
+  <div class="dropdown" ref="dropdownRef">
+    <a
+      href="#"
+      class="btn btn-outline-light my-2 dropdown-toggle"
+      @click.prevent="toggleOpen"
+    >
+      {{ title }}
     </a>
-    <ul class="dropdown-menu" :style="{display:'block'}" v-if="isOpen">
+    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
       <!-- <li><a class="dropdown-item" href="#">新建文章</a></li>
       <li><a class="dropdown-item" href="#">编辑资料</a></li> -->
       <slot></slot>
@@ -11,8 +15,8 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-
+import { defineComponent, ref, watch } from 'vue'
+import useClickOutside from '../hooks/useClickOutside'
 export default defineComponent({
   name: 'Dropdown',
   props: {
@@ -21,14 +25,40 @@ export default defineComponent({
       required: true
     }
   },
-  setup () {
+  // eslint-disable-next-line space-before-function-paren
+  setup() {
     const isOpen = ref(false)
+    const dropdownRef = ref<null | HTMLElement>(null)
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    const isClickOutside = useClickOutside(dropdownRef)
+
+    watch(isClickOutside, () => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
+      }
+    })
+    // const handler = (e: MouseEvent) => {
+    //   if (dropdownRef.value) {
+    //     if (
+    //       !dropdownRef.value.contains(e.target as HTMLElement) &&
+    //       isOpen.value
+    //     ) {
+    //       isOpen.value = false
+    //     }
+    //   }
+    // }
+    // onMounted(() => {
+    //   document.addEventListener('click', handler)
+    // })
+    // onUnmounted(() => {
+    //   document.removeEventListener('click', handler)
+    // })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownRef
     }
   }
 })
